efc6fe4e73343e83957033ca88d31e8fee04ea68
richard lawson <lawson_richard@hotmail.com>
2015-09-04 20:03:16 +0100

    - added new home page - moved game templates to 3 level inheritance - added username to all game pages

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,11 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));
+    	if($this->get('security.context')->isGranted('ROLE_USER')){
+    		return $this->redirectToRoute('game_home');
+    	}else{
+    		return $this->render('default/index.html.twig', array());
+    	}
+        
     }
 }
